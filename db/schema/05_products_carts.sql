0fbf5a0c5f59b109f89f7b140d08dabbb4a6e3a7
juliasut <juliasutygina@gmail.com>
2021-07-25 20:56:34 -0600

    Remove duplicates

--- db/schema/05_products_carts.sql
+++
@@ -1,9 +0,0 @@
--- Drop and recreate Widgets table (Example)
-DROP TABLE IF EXISTS products_carts CASCADE;
-
-CREATE TABLE products_carts (
-  id SERIAL PRIMARY KEY NOT NULL,
-  cart_id INTEGER REFERENCES carts(id) ON DELETE CASCADE,
-  product_id INTEGER REFERENCES products(id),
-  quantity INT NOT NULL DEFAULT 1
-);
--- db/seeds/06_carts.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO carts (user_id)
-VALUES
-(1),
-(2),
-(3),
-(4),
-(5),
-(6),
-(7);

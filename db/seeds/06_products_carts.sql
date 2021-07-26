3bf3eb79c4e4a8b13d964ec7482c3e2978baac8b
juliasut <juliasutygina@gmail.com>
2021-07-25 20:53:59 -0600

    Fix references

---
+++ db/seeds/06_products_carts.sql
@@ -0,0 +1,12 @@
+-- Widgets table seeds here (Example)
+INSERT INTO products_carts (cart_id, product_id, quantity)
+VALUES
+(1, 2, 1),
+(2, 2, 1),
+(3, 2, 1),
+(4, 2, 1),
+(5, 2, 1),
+(6, 2, 1),
+(7, 2, 1),
+(8, 2, 1);
+
